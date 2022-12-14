27eb7cd541a9acbdec107f071184e331ff4be92d
Al-amin <gbolagade63@gmail.com>
2022-12-14 04:36:40 -0800

    added 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the alphabets in lowercase
+ * alphabets are stored in Alf
+ * return: 0 (success)
+ */
+
+int main(void)
+{
+
+	char Alf;
+
+	for (Alf = 'a'; Alf <= 'z'; Alf++)
+		putchar(Alf);
+	return (0);
+
+}
