a85f3fb8a14acdad9386cdac642037544da867e7
Al-amin <gbolagade63@gmail.com>
2022-12-21 04:41:14 -0800

    added 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,17 @@
  */
 
 int main(void)
-
 {
-	char text[9] = "_putchar";
-	int i = 0;
-	for (i = 0; i < 8; i++)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 }
-		_putchar(text[i]);
-{
-			_putchar('\n');
-			
+	_putchar('\n');
 	return (0);
 }
