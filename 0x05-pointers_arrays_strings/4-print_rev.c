e3f23a3e65bdf2b7a341cd6db6bc1e477179c828
Al-amin <gbolagade63@gmail.com>
2023-04-04 09:51:32 -0700

    added 4-print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints alphabets in reverse
+ * @s: string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (s[i] >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
