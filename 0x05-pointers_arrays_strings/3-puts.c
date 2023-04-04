08727723a505d42edfa0e3aea2a581b1992d9e9d
Al-amin <gbolagade63@gmail.com>
2023-04-04 09:24:19 -0700

    added 3-put

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints string followed by a new line
+ * @str: pointer/string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
